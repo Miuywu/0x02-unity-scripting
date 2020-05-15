c315d19f4bc2a853cc21c211dd573625d6e6cffe
Miuywu <859@holbertonschool.com>
2020-05-14 20:48:13 -0700

    player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+    private int score = 0;
+    public int health = 5;
+
+    private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+
+	private void FixedUpdate()
+	{
+		float moveX = Input.GetAxis("Horizontal");
+		float moveZ = Input.GetAxis("Vertical");
+
+		Vector3 traj = new Vector3(moveX, 0.0f, moveZ);
+		rb.AddForce(traj * speed);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            score++;
+            Debug.Log("Score: " + score.ToString());
+            other.gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("Trap"))
+        {
+            health--;
+            Debug.Log("Health: " + health.ToString());
+        }
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("You win!");
+        }
+    }
+
+    private void Update()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(0);
+        }
+    }
+}
